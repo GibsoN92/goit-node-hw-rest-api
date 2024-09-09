00b69f44c95a11454afa671f556980f02a8edbe5
GibsoN92 <52374403+GibsoN92@users.noreply.github.com>
2024-09-09 20:56:02 +0200

    v1

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require("./routes/api/contacts");
+const usersRouter = require("./routes/api/users");
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/contacts', contactsRouter)
+app.use("/api/contacts", contactsRouter);
+app.use("/api/users", usersRouter);
+
+const publicPath = path.join(__dirname, "public", "avatars");
+app.use("/avatars", express.static(publicPath));
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+const { DB_HOST } = process.env;
+
+mongoose
+  .connect(DB_HOST, {})
+  .then(() => {})
+  .catch((err) => {
+    console.error("Database connection error", err);
+    process.exit(1);
+  });
+
+module.exports = app;
---
+++ routes/api/users.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const {
+  signup,
+  login,
+  logout,
+  current,
+  updateAvatar,
+} = require("../../controllers/users");
+const authMiddleware = require("../../authMiddleware");
+const upload = require("../../middlewares/upload");
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.get("/logout", authMiddleware, logout);
+router.get("/current", authMiddleware, current);
+router.patch("/avatars", authMiddleware, upload.single("avatar"), updateAvatar);
+
+module.exports = router;
